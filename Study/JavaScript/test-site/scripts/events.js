517751fd249dd6b67ecfa206a848a98c77466e58
MakStef <stevaniwwasil@gmail.com>
2022-01-02 15:57:19 +0200

    Changed few files

--- Study/JavaScript/test-site/scripts/events.js
+++ Study/JavaScript/test-site/scripts/events.js
@@ -1,12 +1,19 @@
 /* 
 element.addEventListener(<тип>, <функція>, <опції>) - створює прослуховувач подій, котрому задається тип прослуховування, функція, котрій будуть передані дані і (необов'язково) деякі опції.
 */
-myFunc = (toPrint) => console.log(toPrint);
-let myButton = document.querySelector("#myButton1");
-myButton.addEventListener("click", myFunc);
-
+myFunc = () => console.log('myFunc');
+const myButton = document.querySelector("#myButton");
+myButton.addEventListener('click', myFunc);
+myButton.addEventListener('click', function () { console.log('anon1') });
+myButton.addEventListener('click', function () { console.log('anon2') });
 /*
 element.onclick = <функція> - обробник для нажимання на елемент. Може приймати як задану функцію, так і анонімну.
 */
-myButton = document.querySelector("#myButton2");
-myButton.onclick = myFunc;
+myButton.onclick = myFunc; //Задаємо для onclick функцію myFunc.
+myButton.onclick = function () { console.log('anon3') }; //Перезаписуємо функцію для onclick, в результаті відтвориться не myFunc, а третя анонімна функція.
+
+//Контент input-у буде виведено в showInput
+function showInput() {myDiv.innerText = myInput.value}
+const myInput = document.querySelector('#myInput')
+const myDiv = document.querySelector('#showInput')
+myInput.addEventListener('input', showInput)
